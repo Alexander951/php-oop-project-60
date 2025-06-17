39f395b58ca3ed681ec319b7c2c5280302089a68
Александр <@10.8.0.102>
2025-06-18 03:39:30 +0500

    fix: staticMethod.dynamicCall

--- tests/StringValidatorTest.php
+++ tests/StringValidatorTest.php
@@ -3,6 +3,7 @@
 namespace Hexlet\Validator\Tests;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
 use Hexlet\Validator\Validator;
 
 class StringValidatorTest extends TestCase
@@ -18,9 +19,9 @@ class StringValidatorTest extends TestCase
     {
         $schema = $this->validator->string();
 
-        $this->assertTrue($schema->isValid(''));
-        $this->assertTrue($schema->isValid(null));
-        $this->assertTrue($schema->isValid('what does the fox say'));
+        Assert::assertTrue($schema->isValid(''));
+        Assert::assertTrue($schema->isValid(null));
+        Assert::assertTrue($schema->isValid('what does the fox say'));
     }
 
     public function testIndependentSchemas(): void
@@ -30,33 +31,33 @@ class StringValidatorTest extends TestCase
 
         $schema1->required();
 
-        $this->assertFalse($schema1->isValid(''));
-        $this->assertTrue($schema2->isValid(''));
+        Assert::assertFalse($schema1->isValid(''));
+        Assert::assertTrue($schema2->isValid(''));
     }
 
     public function testRequired(): void
     {
         $schema = $this->validator->string()->required();
 
-        $this->assertFalse($schema->isValid(null));
-        $this->assertFalse($schema->isValid(''));
-        $this->assertTrue($schema->isValid('hexlet'));
+        Assert::assertFalse($schema->isValid(null));
+        Assert::assertFalse($schema->isValid(''));
+        Assert::assertTrue($schema->isValid('hexlet'));
     }
 
     public function testContains(): void
     {
         $schema = $this->validator->string()->contains('what');
 
-        $this->assertTrue($schema->isValid('what does the fox say'));
-        $this->assertFalse($schema->isValid('hello world'));
+        Assert::assertTrue($schema->isValid('what does the fox say'));
+        Assert::assertFalse($schema->isValid('hello world'));
     }
 
     public function testMinLength(): void
     {
         $schema = $this->validator->string()->minLength(5);
 
-        $this->assertTrue($schema->isValid('Hexlet'));
-        $this->assertFalse($schema->isValid('Hex'));
+        Assert::assertTrue($schema->isValid('Hexlet'));
+        Assert::assertFalse($schema->isValid('Hex'));
     }
 
     public function testValidationPriority(): void
@@ -65,7 +66,7 @@ class StringValidatorTest extends TestCase
             ->minLength(10)
             ->minLength(5);
 
-        $this->assertTrue($schema->isValid('Hexlet'));
+        Assert::assertTrue($schema->isValid('Hexlet'));
     }
 
     public function testCombinedValidators(): void
@@ -75,11 +76,11 @@ class StringValidatorTest extends TestCase
             ->contains('hex')
             ->minLength(5);
 
-        $this->assertTrue($schema->isValid('hexlet'));
-        $this->assertFalse($schema->isValid('let')); // Too short
-        $this->assertFalse($schema->isValid('hello')); // Doesn't contain 'hex'
-        $this->assertFalse($schema->isValid('')); // Empty
-        $this->assertFalse($schema->isValid(null)); // Null
+        Assert::assertTrue($schema->isValid('hexlet'));
+        Assert::assertFalse($schema->isValid('let')); // Too short
+        Assert::assertFalse($schema->isValid('hello')); // Doesn't contain 'hex'
+        Assert::assertFalse($schema->isValid('')); // Empty
+        Assert::assertFalse($schema->isValid(null)); // Null
     }
 
     public function testCustomStringValidator(): void
@@ -88,8 +89,8 @@ class StringValidatorTest extends TestCase
         $v->addValidator('string', 'startWith', fn($value, $start) => str_starts_with($value, $start));
 
         $schema = $v->string()->test('startWith', 'H');
-        $this->assertFalse($schema->isValid('exlet'));
-        $this->assertTrue($schema->isValid('Hexlet'));
-        $this->assertTrue($schema->isValid(null)); // null allowed by default
+        Assert::assertFalse($schema->isValid('exlet'));
+        Assert::assertTrue($schema->isValid('Hexlet'));
+        Assert::assertTrue($schema->isValid(null)); // null allowed by default
     }
 }
